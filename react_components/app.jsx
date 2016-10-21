8be67d98b0b88e61024d053a11ac58609ced3249
yunhsincynthiachen <yun-hsin.chen@students.olin.edu>
2016-10-21 10:45:59 -0400

    with basic react stuff

---
+++ react_components/app.jsx
@@ -0,0 +1,72 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var DisplayEnum = Object.freeze({
+    DISPLAY_HOME: 0
+});
+
+
+var DestinyChildBox = React.createClass({
+
+    getInitialState: function() {
+        return {
+            display: DisplayEnum.DISPLAY_HOME,
+            resources: [],
+            metadata: {}
+        };
+    },
+
+    componentDidMount: function() {
+        //Calls login to facebook as soon as component is mounting
+        // this.getResourcesPlusMetadata();
+        return null;
+    },
+
+    // getResourcesPlusMetadata: function() {
+    //   console.log("hello");
+    //   $.ajax({
+    //       url: '/api/resources/allResources',
+    //       dataType: 'json',
+    //       type: 'GET',
+    //       success: function(resourceAndMetadataItems){
+    //         console.log("hello" + resourceAndMetadataItems);
+    //           this.setState({
+    //             resources: resourceAndMetadataItems['resources'],
+    //             metadata: resourceAndMetadataItems['metadata']
+    //           });
+    //
+    //       }.bind(this),
+    //       error: function(xhr, status, err){
+    //           console.log("cannot get resources, '/api/resources/allResources'", status, err.toString());
+    //       }.bind(this)
+    //   })
+    // },
+
+    render: function() {
+        var page;
+
+        // Decide whether to show login page, tinder news wheel, or dashboard
+        switch (this.state.display) {
+
+            case DisplayEnum.DISPLAY_HOME:
+                page = (
+                  <div>
+                    <h1>POE DDR</h1>
+                  </div>
+              );
+              break;
+
+      }
+
+      return (
+          <div>
+      {page}
+          </div>
+      );
+    }
+});
+
+ReactDOM.render(
+  <DestinyChildBox />,
+  document.getElementById('content')
+);
