1300bb8733facec8582d34eab37a8af18eabe352
Casey Alvarado <alvarado.casey@gmail.com>
2016-10-26 20:29:43 -0400

    adding button output

--- Arduino/button_press_protoype/button_press_protoype.ino
+++ Arduino/button_press_protoype/button_press_protoype.ino
@@ -14,57 +14,60 @@ void setup(){
 void loop(){
  //when button is pressed, it completes the circuit of and "pulls down" 
   int buttonOpen = digitalRead(buttonPin); 
-  Serial.println(buttonOpen);
   currentState = buttonOpen; 
   
-//  if (!buttonOpen){ 
+  if (!buttonOpen){ 
+    Serial.print("open 1");
+    digitalWrite(LEDout, HIGH);
+    delay(400);
+    digitalWrite(LEDout, LOW); 
+    Serial.println("close 1");  
+  }
+  
+//  //if it is recently pressed, then set the time as the "first time" aka the zeroth millisecond 
+//  //this will help compare how long button has been pressed
+//  if ((currentState == LOW) && (prevState == HIGH)){
+//    Serial.println("PRESSED");
+//    firstTime = millis(); 
+//    prevState = LOW;
+//   
 //    digitalWrite(LEDout, HIGH);
 //    delay(200);
-//    digitalWrite(LEDout, LOW); 
-//    Serial.print("whatever letter here"); 
+//    digitalWrite(LEDout, LOW);  
+//    delay(200); 
+//    
+//    
 //  }
-  
-  //if it is recently pressed, then set the time as the "first time" aka the zeroth millisecond 
-  //this will help compare how long button has been pressed
-  if ((currentState == LOW) && (prevState == HIGH)){
-    Serial.println("PRESSED");
-    firstTime = millis(); 
-    prevState = LOW;
-   
-    digitalWrite(LEDout, HIGH);
-    delay(200);
-    digitalWrite(LEDout, LOW);  
-    delay(200); 
-  }
-  
-  //calculate how long the button has been held 
-  long timeHeld = millis() - firstTime; 
-  Serial.println("first time pressed"); 
-  Serial.println(firstTime); 
-  Serial.println(timeHeld);
-   
-  //if the button has been held longer than some time (for noise purposes), 
-  if((timeHeld > 50) && (firstTime !=0)){ 
-    Serial.println("IN here"); 
-    Serial.println("modulo");
-    Serial.println(timeHeld%1000); 
-    delay(200);
-    
-    if(currentState == LOW){ 
-      if ((timeHeld%1000 >=0) && (timeHeld%1000 <=50)){ 
-        Serial.println("one second has passed");
-        digitalWrite(LEDout, HIGH);
-        delay(500);
-        digitalWrite(LEDout, LOW);
-       delay(200); 
-      }
-    }    
-    if((currentState == HIGH) && (prevState == LOW)){
-       digitalWrite(LEDout, LOW);  
-       Serial.println("unpress");  
-       prevState = currentState; 
-       firstTime = 0; 
-    }
-  }
+//  
+//  //calculate how long the button has been held 
+//  long timeHeld = millis() - firstTime; 
+//  Serial.println("first time pressed"); 
+//  Serial.println(firstTime); 
+//  Serial.println(timeHeld);
+//  delay(500); 
+//   
+//  //if the button has been held longer than some time (for noise purposes), 
+//  if((timeHeld > 20) && (firstTime !=0)){ 
+//    Serial.println("IN here"); 
+//    Serial.println("modulo");
+//    Serial.println(timeHeld%1000); 
+//    delay(100);
+//    
+//    if(currentState == LOW){ 
+//      if ((timeHeld%1000 >=0) && (timeHeld%1000 <=50)){ 
+//        Serial.println("one second has passed");
+//        digitalWrite(LEDout, HIGH);
+//        delay(500);
+//        digitalWrite(LEDout, LOW);
+//       delay(200); 
+//      }
+//    }    
+//    if((currentState == HIGH) && (prevState == LOW)){
+//       digitalWrite(LEDout, LOW);  
+//       Serial.println("unpress");  
+//       prevState = currentState; 
+//       firstTime = 0; 
+//    }
+//  }
   
 }
