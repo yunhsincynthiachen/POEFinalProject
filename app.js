bbbfdc1b3519b87be1ebcc718252fc8fa65491cc
yunhsincynthiachen <yun-hsin.chen@students.olin.edu>
2016-11-08 10:23:46 -0500

    added gsap and added the web game

--- app.js
+++ app.js
@@ -7,24 +7,24 @@ var io = require('socket.io').listen(server);
 
 app.use(express.static(__dirname + '/public'));
 
-var serialport = new SerialPort("/dev/ttyACM1"); // replace this address with your port address
-serialport.on('open', function(){
-	// Now server is connected to Arduino
-	console.log('Serial Port Opened');
-
-	var lastValue;
-	io.sockets.on('connection', function (socket) {
-		//Connecting to client
-		console.log('Socket connected');
-		socket.emit('connected');
-		var lastValue;
-
-		serialport.on('data', function(data){
-			var step = data[0];
-			if(lastValue !== step){
-				socket.emit('data', step);
-			}
-			lastValue = step;
-		});
-	});
-});
+// var serialport = new SerialPort("/dev/ttyACM0"); // replace this address with your port address
+// serialport.on('open', function(){
+// 	// Now server is connected to Arduino
+// 	console.log('Serial Port Opened');
+//
+// 	var lastValue;
+// 	io.sockets.on('connection', function (socket) {
+// 		//Connecting to client
+// 		console.log('Socket connected');
+// 		socket.emit('connected');
+// 		var lastValue;
+//
+// 		serialport.on('data', function(data){
+// 			var step = data[0];
+// 			if(lastValue !== step){
+// 				socket.emit('data', step);
+// 			}
+// 			lastValue = step;
+// 		});
+// 	});
+// });
--- public/script.js
+++ public/script.js
@@ -1,10 +1,69 @@
-socket.on('data', function (data) {
-	var box = document.getElementById('step' + String(data).charAt(0));
+// socket.on('data', function (data) {
+// 	var box = document.getElementById('step' + String(data).charAt(0));
+//
+// 	if (String(data).charAt(1) == 0) {
+// 		box.style.backgroundColor = 'black';
+// 	} else {
+// 		box.style.backgroundColor = '#800080';
+// 	}
+// 	step = data;
+// });
 
-	if (String(data).charAt(1) == 0) {
-		box.style.backgroundColor = 'black';
+var map = {37: false, 38: false, 40: false, 39: false};
+var stepKey = {"left":37, "up":38, "down":40, "right":39};
+var stepReverseKey = {37:"left", 38:"up", 40:"down", 39:"right"};
+
+var stepOrder = ["left", "right", "up", "down", "up", "left"];
+var stepNum = 0;
+var myVar = '<div class="col-md-3 moving '+stepOrder[stepNum]+'" id="'+stepOrder[stepNum]+'"></div>';
+var rowDiv = $('#row-moving');
+
+rowDiv.append(myVar);
+
+var shuttle1 = $('.'+stepOrder[stepNum]);
+
+var complete = function () {
+	if (map[stepKey[stepOrder[stepNum]]]) {
+		$("#score").text(parseInt($("#score").text()) + 100);
+		$('#scoreChange').text('+100').css({'color':'green'});
 	} else {
-		box.style.backgroundColor = '#800080';
+		$("#score").text(parseInt($("#score").text()) - 100);
+		$('#scoreChange').text('-100').css({'color':'red'});
 	}
-	step = data;
+
+	stepNum++;
+	var myVar2 = '<div class="col-md-3 moving '+stepOrder[stepNum]+'" id="'+stepOrder[stepNum]+'"></div>';
+	rowDiv.append(myVar2);
+	var shuttle1 = $('.'+stepOrder[stepNum]);
+
+	if (stepNum == stepOrder.length) {
+		var moving_arrow = TweenLite.to(shuttle1, 1, {
+			y: "-425px",
+			ease: Linear.easeNone,
+		});
+	} else {
+		var moving_arrow = TweenLite.to(shuttle1, 1, {
+			y: "-425px",
+			ease: Linear.easeNone,
+			onComplete: complete,
+		});
+	}
+};
+
+var moving_arrow = TweenLite.to(shuttle1, 1, {
+  y: "-425px",
+  ease: Linear.easeNone,
+	onComplete: complete,
+});
+
+$(document).keydown(function(e) {
+    if (e.keyCode in map) {
+        map[e.keyCode] = true;
+				$('#' + stepReverseKey[e.keyCode]).css({"backgroundColor": "black"});
+    }
+}).keyup(function(e) {
+    if (e.keyCode in map) {
+        map[e.keyCode] = false;
+				$('#' + stepReverseKey[e.keyCode]).css({"backgroundColor": "#800080"});
+    }
 });
